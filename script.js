fa665edf0643dc7490615b13a2d8fcbaa8823586
Syahreza <syahrezafistiferdian32@gmail.com>
2023-06-01 20:00:56 +0700

    Update

---
+++ script.js
@@ -0,0 +1,16 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('menu-active');
+};
+
+const cekElemen = new IntersectionObserver(elemenSingDicek => {
+    elemenSingDicek.forEach(cek => {
+        if(cek.isIntersecting){
+            document.querySelector(".hp").classList.add("fadeInLeft");
+            document.querySelector(".rs").classList.add("fadeInRight");
+        }
+    })
+})
+
+cekElemen.observe(document.querySelector(".resep"));
